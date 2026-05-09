5d397a8c795ccfbecfc0ca984258c264e7b1bc41
vaidehi <vaideheekumari2802@gamil.com>
2026-05-09 05:55:09 +0530

    Initial Commit

---
+++ expense-tracker-devops/expense-tracker-devops/src/main/java/com/expense/expense_tracker_devops/ExpenseTrackerDevopsApplication.java
@@ -0,0 +1,13 @@
+package com.expense.expense_tracker_devops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseTrackerDevopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseTrackerDevopsApplication.class, args);
+	}
+
+}
---
+++ expense-tracker-devops/expense-tracker-devops/src/test/java/com/expense/expense_tracker_devops/ExpenseTrackerDevopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.expense.expense_tracker_devops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseTrackerDevopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
