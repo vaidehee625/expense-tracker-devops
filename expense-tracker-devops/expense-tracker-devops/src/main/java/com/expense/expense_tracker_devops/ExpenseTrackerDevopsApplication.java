2b407605c743477f92b4ce4c7365069d32fc20d3
vaidehi <vaideheekumari2802@gamil.com>
2026-05-09 09:36:11 +0530

    Configured Dockerized Spring Boot DevOps project

--- expense-tracker-devops/expense-tracker-devops/src/main/java/com/expense/expense_tracker_devops/ExpenseTrackerDevopsApplication.java
+++ expense-tracker-devops/expense-tracker-devops/src/main/java/com/expense/expense_tracker_devops/ExpenseTrackerDevopsApplication.java
@@ -2,12 +2,19 @@ package com.expense.expense_tracker_devops;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ExpenseTrackerDevopsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ExpenseTrackerDevopsApplication.class, args);
 	}
 
+	@GetMapping("/")
+	public String home() {
+		return "Expense Tracker DevOps App Running Successfully!";
+	}
 }
